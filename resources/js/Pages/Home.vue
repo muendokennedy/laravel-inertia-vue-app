accd54536328da1086e374cb24d92009e4984a39
muendokennedy <kennedymunyao999@gmai.com>
2024-05-29 16:10:24 +0300

    Added some code block to preserve the scroll of a particular screen when a link at the bottom of the screen is clicked

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,6 +5,7 @@
         <Head :title="` | ${$page.component}`"/>
         {{ $page.props.user }}
         <h2 class="text-2xl font-semibold">Hello from inertia home</h2>
+        <Link class="mt-[1200px] block" href="/" preserve-scroll>Refresh</Link>
 </template>
 <style scoped>
 </style>
