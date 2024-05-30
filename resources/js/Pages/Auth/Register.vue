8b78bbabcc61fba121f8baf0c503f4bfce66ea60
muendokennedy <kennedymunyao999@gmai.com>
2024-05-30 11:26:46 +0300

    Extracted all the code inside the input wrapper into a separate component and parsed the necessary props to the Text input component

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,17 +1,18 @@
 <script setup>
-import { reactive } from 'vue';
 import { useForm } from '@inertiajs/vue3';
+import TextInput from '../components/TextInput.vue'
 
 const form = useForm({
     name: null,
     email: null,
     password: null,
-    password_confirmation: null
+    password_confirmation: null,
 });
+
 
 const onSubmit = () => {
     form.post(route('register'), {
-        onError: () => form.reset('password', 'password_confirmation')
+        onError: () => form.reset('password_confirmation')
     });
 }
 
@@ -21,25 +22,10 @@ const onSubmit = () => {
     <h1 class="title">Register a new account</h1>
     <div class="w-2/4 mx-auto">
         <form @submit.prevent="onSubmit">
-            <div class="mb-6">
-                <label>Name</label>
-                <input type="text" v-model="form.name">
-                <small class="error">{{ form.errors.name }}</small>
-            </div>
-            <div class="mb-6">
-                <label>Email</label>
-                <input type="text" v-model="form.email">
-                <small class="error">{{ form.errors.email }}</small>
-            </div>
-            <div class="mb-6">
-                <label>Password</label>
-                <input type="password" v-model="form.password">
-                <small class="error">{{ form.errors.password }}</small>
-            </div>
-            <div class="mb-6">
-                <label>Confirm Password</label>
-                <input type="password" v-model="form.password_confirmation">
-            </div>
+            <TextInput name="Enter name:" v-model="form.name" :errorMessage="form.errors.name"/>
+            <TextInput name="Enter Email:" v-model="form.email" :errorMessage="form.errors.email"/>
+            <TextInput name="Enter Password:" type="password" v-model="form.password" :errorMessage="form.errors.password"/>
+            <TextInput name="Confirm Password:" type="password" v-model="form.password_confirmation"/>
             <div>
                 <p class="text-slate-600 mb-2">Already a user? <a href="#" class="text-link">Login</a></p>
                 <button class="primary-btn" :disabled="form.processing">Register</button>
@@ -47,3 +33,4 @@ const onSubmit = () => {
         </form>
     </div>
 </template>
+
---
+++ resources/js/Pages/components/TextInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+const model = defineModel({
+    type: null,
+    required: true
+})
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+        default: 'text'
+    },
+    errorMessage: String
+})
+</script>
+
+<template>
+    <div class="mb-6">
+        <label>{{  name }}</label>
+        <input :type="type" v-model="model" :class="{'!ring-red-500' : errorMessage}">
+        <small class="error" v-if="errorMessage">{{ errorMessage }}</small>
+    </div>
+</template>
